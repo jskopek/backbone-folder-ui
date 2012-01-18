e66bdb07aad824c7378d24906135814aacb1cc50
Jean-Marc Skopek <jskopek@gmail.com>
2012-01-18 17:03:17 -0500

    Fixed rendering folder problems

--- tree.js
+++ tree.js
@@ -11,9 +11,12 @@ var FolderView = Backbone.View.extend({
     tagName: "li",
     className: "folder",
 
+    initialize: function() {
+        this.render();
+    },
     render: function() {
-        var template = _.template( $("#folder_template").html() );
-        var html = this.template( this.model.toJSON() );
+        var template = _.template( $("#folder_template").text() );
+        var html = template( this.model.toJSON() );
         $(this.el).html(html);
     }
 });
