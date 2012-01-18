909d35d956bcd477399a3a924288a51c483666ff
Jean-Marc Skopek <jskopek@gmail.com>
2012-01-18 17:50:58 -0500

    Hackalicious folder toggling

--- tree.js
+++ tree.js
@@ -25,7 +25,8 @@ var MIView = Backbone.View.extend({
 var Folder = Backbone.Model.extend({
     defaults: {
         title: "",
-        children: new Backbone.Collection()
+        children: new Backbone.Collection(),
+        hidden: false
     },
     initialize: function() {
         if( _.isArray( this.get("children") ) ) {
@@ -41,18 +42,34 @@ var FolderView = Backbone.View.extend({
 
     initialize: function() {
         this.render();
+        this.model.bind("change:hidden", this.render, this);
+    },
+    toggle_hide: function(e) {
+        e.preventDefault();
+        var is_hidden = this.model.get("hidden");
+        this.model.set({ "hidden": !is_hidden });
     },
     render: function() {
-        var template = _.template("<b><%= title %></b><ul></ul>");
-        var html = template( this.model.toJSON() );
-        $(this.el).html(html);
-        var ul_el = $(this.el).find("ul");
-
-        this.model.get("children").each(function(child) {
-            var li = $("<li></li>");
-            li.html( child.get("view").el );
-            ul_el.append(li);
+        var template = _.template("<div id='<%= cid %>'><b><%= title %></b><a href='#'>Hide</a></div><ul></ul>");
+        var html = template({
+           "cid": this.model.cid,
+           "title": this.model.get("title")
         });
+        $(this.el).html(html);
+
+        //bind hide event
+        $(this.el).find("#" + this.model.cid + " a").click($.proxy(function(e) { this.toggle_hide(e); }, this));
+
+        //loop through and add children
+        var ul_el = $(this.el).find("ul");
+        if( !this.model.get("hidden") ) {
+            this.model.get("children").each(function(child) {
+                var li = $("<li></li>");
+                child.get("view").render(); //rebinds hide event
+                li.html( child.get("view").el );
+                ul_el.append(li);
+            });
+        }
     }
 });
 
