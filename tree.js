dd7e0a90725a9b28e94714ad3c4d727e803569e1
Jean-Marc Skopek <jskopek@gmail.com>
2012-01-18 17:26:05 -0500

    Proper styling of simple tree

--- tree.js
+++ tree.js
@@ -1,9 +1,34 @@
-var MI = Backbone.Model.extend({});
+var MIcounter = 0;
+var MI = Backbone.Model.extend({
+    initialize: function() {
+        //temp way of setting MI name really quickly
+        this.set({"title": "Module Item " + MIcounter});
+        MIcounter++;
+
+        this.set({"view": new MIView({ model: this }) });
+    }
+});
+
+var MIView = Backbone.View.extend({
+    className: "module_item",
+    tagName: "li",
+
+    initialize: function() {
+        this.render();
+    },
+    render: function() {
+        var template = _.template("<b>MI: <%= title %></b>");
+        $(this.el).html( template( this.model.toJSON() ) );
+    }
+})
 
 var Folder = Backbone.Model.extend({
     defaults: {
         title: "",
         children: []
+    },
+    initialize: function() {
+        this.set({"view": new FolderView({ model: this }) });
     }
 });
 
@@ -15,9 +40,18 @@ var FolderView = Backbone.View.extend({
         this.render();
     },
     render: function() {
-        var template = _.template( $("#folder_template").text() );
+        var template = _.template("<b>TREE: <%= title %></b><ul></ul>");
         var html = template( this.model.toJSON() );
         $(this.el).html(html);
+        var ul_el = $(this.el).find("ul");
+
+        console.log( this.model.get("title"), this.model.get("children") );
+        _.each( this.model.get("children"), function(child) {
+            var li = $("<li></li>");
+            li.html( child.get("view").el );
+            ul_el.append(li);
+            console.log("LI", li.html(), ul_el);
+        });
     }
 });
 
