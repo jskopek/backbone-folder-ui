e3189b3dafe856a2c649a5c615bae9c09dad3899
Jean-Marc Skopek <jskopek@gmail.com>
2012-02-20 17:55:36 -0500

    Added more folder model test cases

--- test/testTreeFolder.js
+++ test/testTreeFolder.js
@@ -1,3 +1,14 @@
+var commonFolderStructure = {
+    "title": "Folder A",
+    "children": [
+        new TreeItem({ "title": "Child item 1" }),
+        new Folder({ 
+            "title": "Child item 2",
+            "children": [new TreeItem({ "title": "Child item 3" })]
+        })
+    ]
+}
+
 $(document).ready(function() {
     module("Tree Folder");
     test("initialize without children", function() {
@@ -19,16 +30,7 @@ $(document).ready(function() {
         ok( f.get("children").last() instanceof Folder );
     });
     test("tree serialize", function() {
-        var f = new Folder({
-            "title": "Folder A",
-            "children": [
-                new TreeItem({ "title": "Child item 1" }),
-                new Folder({ 
-                    "title": "Child item 2",
-                    "children": [new TreeItem({ "title": "Child item 3" })]
-                })
-            ]
-        });
+        var f = new Folder(commonFolderStructure);
 
         var data = f.serialize();
 
@@ -46,16 +48,7 @@ $(document).ready(function() {
         equal( data.children[1].children.length, 1 );
     });
     test("tree deserialize", function() {
-        var f = new Folder({
-            "title": "Folder A",
-            "children": [
-                new TreeItem({ "title": "Child item 1" }),
-                new Folder({ 
-                    "title": "Child item 2",
-                    "children": [new TreeItem({ "title": "Child item 3" })]
-                })
-            ]
-        });
+        var f = new Folder(commonFolderStructure);
 
         var data = f.serialize();
 
@@ -71,19 +64,96 @@ $(document).ready(function() {
         equal(f2.get("children").last().get("children").length, 1);
     });
     test("folder selected changes children selected", function() {
-        undeclared();
+        var f = new Folder(commonFolderStructure);
+        f.nested_each(function(i) { i.set({"selectable": true}) });
+
+        //check that all items are deselected
+        var is_selected_list = f.flatten().map(function(i) { return i.is_selected(); });
+        deepEqual( is_selected_list, [false, false, false, false] );
+
+        //select root folder and ensure that all children are selected
+        f.set({"selected": true});
+        var is_selected_list = f.flatten().map(function(i) { return i.is_selected(); });
+        deepEqual( is_selected_list, [true, true, true, true] );
+
+        //deselect root folder and ensure all children are unselected
+        f.set({"selected": false});
+        var is_selected_list = f.flatten().map(function(i) { return i.is_selected(); });
+        deepEqual( is_selected_list, [false, false, false, false] );
+        
+        //set the item that is the child of the child folder to true
+        //will set it's parent's folder selected status to true, and the root folder's
+        //status to mixed (but false in is_selected)
+        f.flatten().last().set({"selected": true});
+        var is_selected_list = f.flatten().map(function(i) { return i.is_selected(); });
+        deepEqual( is_selected_list, [false, false, true, true] );
+        equal( f.get("selected"), "mixed" );
     });
-    test("folder selected without children", function() {
+    test("moving selected item between folders changed folder selection", function() {
         undeclared();
     });
     test("child added to folder", function() {
-        undeclared();
+        var f = new Folder();
+        equal( f.get('children').length, 0 );
+
+        //add item
+        var i1 = new TreeItem({"title": "Item 1"});
+        f.add(i1);
+        equal( f.get('children').length, 1 );
+
+        //add item at first index
+        var i2 = new TreeItem({"title": "Item 2"});
+        f.add(i2, 0);
+        equal( f.get('children').length, 2 );
+        equal( f.get('children').first().get("title"), "Item 2" );
+
+        //add item at very high index
+        var i3 = new TreeItem({"title": "Item 3"});
+        f.add(i3, 1000);
+        equal( f.get("children").length, 3);
+        equal( f.get("children").at(2).get("title"), "Item 3");
     });
     test("child removed from folder", function() {
-        undeclared();
+        var f = new Folder();
+
+        //add item
+        var i1 = new TreeItem({"title": "Item 1"});
+        f.add(i1);
+        equal( f.get('children').length, 1 );
+
+        //attempt to remove item that is not in folder from folder
+        var i2 = new TreeItem({"title": "Item 2"});
+        f.remove(i2);
+        equal( f.get('children').length, 1 );
+
+        //remove item from folder
+        f.remove(i1);
+        equal( f.get('children').length, 0 );
     });
     test("child moved in folder", function() {
-        undeclared();
+        var f = new Folder();
+
+        //add three children to folder
+        var i1 = new TreeItem({"title": "Item 1"});
+        var i2 = new TreeItem({"title": "Item 2"});
+        var i3 = new TreeItem({"title": "Item 3"});
+        f.add([i1, i2, i3]);
+        equal( f.get('children').length, 3 );
+
+        //move item 1 to last position
+        equal( f.get('children').first().get("title"), "Item 1" );
+        f.move(i1, 1000);
+        equal( f.get('children').first().get("title"), "Item 2" );
+        equal( f.get('children').last().get("title"), "Item 1" );
+
+        //move item 3 to first position
+        f.move(i3, 0);
+        equal( f.get('children').first().get("title"), "Item 3" );
+
+        //move item 3 to middle
+        f.move(i3, 1);
+        equal( f.get('children').first().get("title"), "Item 2" );
+        equal( f.get('children').at(1).get("title"), "Item 3" );
     });
     test("child retrieved from folder", function() {
         undeclared();
