c4838e608cd4a4335497e1e28e04774f388d4755
Jean-Marc Skopek <jskopek@gmail.com>
2012-02-20 16:39:36 -0500

    Added shell for folder test cases

---
+++ test/testTreeFolder.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    module("Tree Folder");
+    test("initialize without children", function() {
+        undeclared();
+    });
+    test("initialize with children", function() {
+        undeclared();
+    });
+    test("tree serialize", function() {
+        undeclared();
+    });
+    test("tree deserialize", function() {
+        undeclared();
+    });
+    test("folder selected changes children selected", function() {
+        undeclared();
+    });
+    test("folder selected without children", function() {
+        undeclared();
+    });
+    test("child added to folder", function() {
+        undeclared();
+    });
+    test("child removed from folder", function() {
+        undeclared();
+    });
+    test("child moved in folder", function() {
+        undeclared();
+    });
+    test("child retrieved from folder", function() {
+        undeclared();
+    });
+    test("child retrieved from nested folders", function() {
+        undeclared();
+    });
+    test("folder can loop through nested children", function() {
+        undeclared();
+    });
+    test("folder can return flattened list of nested children", function() {
+        undeclared();
+    });
+    test("folder can return if it is selected or not", function() {
+        undeclared();
+    });
+});
+
