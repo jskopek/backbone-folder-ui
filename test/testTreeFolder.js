d5aba01f08fd7ace8ca7fa13df3813a4158e2bf2
Jean-Marc Skopek <jskopek@gmail.com>
2012-02-20 17:18:04 -0500

    Started work on folder test cases

--- test/testTreeFolder.js
+++ test/testTreeFolder.js
@@ -1,16 +1,74 @@
 $(document).ready(function() {
     module("Tree Folder");
     test("initialize without children", function() {
-        undeclared();
+        var f = new Folder();
+        equal(f.get('title'), '');
+        equal(f.get('children').length, 0);
     });
     test("initialize with children", function() {
-        undeclared();
+        var f = new Folder({
+            "title": "Folder A",
+            "children": [
+                new TreeItem({ "title": "Child item 1" }),
+                new Folder({ "title": "Child item 2" })
+            ]
+        });
+        equal(f.get("title"), "Folder A");
+        equal(f.get("children").length, 2);
+        ok( f.get("children").first() instanceof TreeItem );
+        ok( f.get("children").last() instanceof Folder );
     });
     test("tree serialize", function() {
-        undeclared();
+        var f = new Folder({
+            "title": "Folder A",
+            "children": [
+                new TreeItem({ "title": "Child item 1" }),
+                new Folder({ 
+                    "title": "Child item 2",
+                    "children": [new TreeItem({ "title": "Child item 3" })]
+                })
+            ]
+        });
+
+        var data = f.serialize();
+
+        //root folder data
+        equal( data.title, "Folder A" );
+        ok( data.children );
+
+        //first child item data
+        equal( data.children[0].constructor, "item" );
+        equal( data.children[0].title, "Child item 1" );
+        
+        //second child folder data
+        equal( data.children[1].constructor, "folder" );
+        equal( data.children[1].title, "Child item 2" );
+        equal( data.children[1].children.length, 1 );
     });
     test("tree deserialize", function() {
-        undeclared();
+        var f = new Folder({
+            "title": "Folder A",
+            "children": [
+                new TreeItem({ "title": "Child item 1" }),
+                new Folder({ 
+                    "title": "Child item 2",
+                    "children": [new TreeItem({ "title": "Child item 3" })]
+                })
+            ]
+        });
+
+        var data = f.serialize();
+
+        //create new folder with deserialized data
+        var f2 = new Folder();
+        f2.deserialize( data );
+
+        //test that deserialization was successful )
+        deepEqual(f.serialize(), f2.serialize());
+        equal(f2.get("title"), "Folder A")
+        ok( f2.get("children").first() instanceof TreeItem );
+        ok( f2.get("children").last() instanceof Folder );
+        equal(f2.get("children").last().get("children").length, 1);
     });
     test("folder selected changes children selected", function() {
         undeclared();
