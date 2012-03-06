33d191ef19ee165784cf73493f917263d577d7eb
Jean-Marc Skopek <jskopek@gmail.com>
2012-03-06 13:04:43 -0500

    Additional test cases

--- test/testTreeItemView.js
+++ test/testTreeItemView.js
@@ -43,6 +43,74 @@ $(document).ready(function() {
     test("title triggers click event when clicked", function() {
         undefined();
     });
+
+    module("Tree Folders");
+    test("children are rendered in folder when they are initialized", function() {
+        var el = $("<div></div>");
+        var folder = new Folder({
+            "children": [
+                new TreeItem({"title": "Item 1"}),
+                new TreeItem({"title": "Item 2"}),
+            ]
+        });
+        var view = new FolderView({"model": folder});
+        $(el).html(view.el);
+
+        equal($(el).find(".folder .tree_row.item:first em").text(), "Item 1");
+        equal($(el).find(".folder .tree_row.item:last em").text(), "Item 2");
+    });
+
+    test("children are rendered in folder as they are added", function() {
+        var el = $("<div></div>");
+        var folder = new Folder();
+        var view = new FolderView({"model": folder});
+        $(el).html(view.el);
+
+        folder.add( new TreeItem({"title": "Item 1"}) );
+        equal($(el).find(".folder .tree_row.item:first em").text(), "Item 1");
+    });
+    test("folder is re-rendered as items moved around", function() {
+        var el = $("<div></div>");
+        var folder = new Folder({
+            "children": [
+                new TreeItem({"title": "Item 1"}),
+                new TreeItem({"title": "Item 2"}),
+            ]
+        });
+        var view = new FolderView({"model": folder});
+        $(el).html(view.el);
+
+        //move Item 1 to position 1
+        folder.move( folder.get("children").at(0), 1 );
+        equal($(el).find(".folder .tree_row.item:first em").text(), "Item 2");
+        equal($(el).find(".folder .tree_row.item:last em").text(), "Item 1");
+    });
+
+    /*module("Tree");*/
+    /*test("dragging item into hidden folder maximizes", function() {*/
+    /*var tree = new Tree({*/
+    /*"chidren": [*/
+    /*new TreeItem({"title": "Item 1"}),*/
+    /*new TreeItem({"title": "Item 2"}),*/
+    /*new TreeItem({"title": "Item 3"})*/
+    /*]*/
+    /*sortable: true*/
+    /*});*/
+    /*var view = new TreeView({"model": tree});*/
+    /*$("body").append(view.el);*/
+
+    /*var folder = new Folder({"title": "Folder 1", "hidden": true});*/
+    /*folder.add( new TreeItem({"title": "Item 1"}) );*/
+    /*folder.add( new TreeItem({"title": "Item 2"}) );*/
+    /*folder.add( new TreeItem({"title": "Item 3"}) );*/
+    /*tree.add(folder);*/
+    /*var i2 = new TreeItem({"title": "Item 4"});*/
+    /*tree.add( i2 );*/
+    /**//*debugger;*/
+    /*tree.move(i2,0);*/
+    /*view.render();*/
+    /*folder.set({"hidden": false});*/
+    /*});*/
 });
 
 function set_common_variables(context, item_value) {
